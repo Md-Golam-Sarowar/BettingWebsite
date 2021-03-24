3cfe2793781dd1319b23365bbe219b0d7101a6c6
Golam Sarowar <golamsarowar@linkstaff.co.jp>
2021-03-24 14:18:18 +0600

    added

--- SportsWebsite/settings.py
+++ SportsWebsite/settings.py
@@ -78,10 +78,10 @@ TEMPLATES = [
 # this is the statics files context path when request the static file in url. for example http://127.0.0.1:8000/static/js/script.js
 STATIC_URL = "/static/"
 
-# this is the static files folder name which you created in django project root folder. This is different from above STATIC_URL.
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "statics"),
-# ]
+this is the static files folder name which you created in django project root folder. This is different from above STATIC_URL.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "statics"),
+]
 WSGI_APPLICATION = "SportsWebsite.wsgi.application"
 
 
