3ddecfe6c3c29b445e4b846a2689940833ea1dd3
defence <iamdefence7@gmail.com>
2021-04-01 03:24:43 +0600

    updated

---
+++ statics/Betting Site_files/showLeagueforTeacher.js
@@ -0,0 +1,32 @@
+window.onload = function ()
+{
+  var allIds = [];
+  ids = document.getElementById("totalids").innerText;
+  splitedids = ids.split(",")
+
+  for(var i=1; i<splitedids.length;i++)
+  {
+    splited = splitedids[i].split("'");
+    allIds.push(splited[1]);
+  }
+
+  payload = {url: allIds};
+
+  $.ajax({
+      url: '../scappingLeagueInfoforTeacher',
+      type: "post",
+      data: payload,
+      dataType: 'json',
+      success: function (returneddata) {
+        if(returneddata["status"] == 200)
+        {
+            document.getElementById("main-container").innerHTML = returneddata["data"];
+        }
+        else if (returneddata["status"] == 500)
+        {
+            alert("No lines available");
+        }
+    }
+    });
+
+}
